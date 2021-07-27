00a241c687ff6f8c87b36244dc9ae8cd9ae4ab06
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-07-27 13:36:26 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -30,7 +30,7 @@ async def start(bot, update):
             await bot.send_document(
                 chat_id=update.chat.id,
                 document = file_id,
-                caption = "❤️𝚃𝚑𝚊𝚗𝚔𝚢𝚘𝚞 𝙵𝚘𝚛 𝚄𝚜𝚒𝚗𝚐 𝙾𝚞𝚛 𝚂𝚎𝚛𝚟𝚒𝚌𝚎 𝙿𝚕𝚎𝚊𝚜𝚎 𝚂𝚞𝚙𝚙𝚘𝚛𝚝 𝚄𝚜 𝙱𝚢 𝚂𝚑𝚊𝚛𝚒𝚗𝚐 𝙾𝚞𝚛 𝙲𝚑𝚊𝚗𝚗𝚎𝚕/𝙶𝚛𝚘𝚞𝚙 𝙻𝚒𝚗𝚔 𝚃𝚘 𝚈𝚘𝚞𝚛 𝙵𝚛𝚒𝚎𝚗𝚍𝚜 \n\n❁𝕁𝕠𝕚𝕟 𝕆𝕦𝕣 ℂ𝕙𝕒𝕟𝕟𝕖𝕝𝕤❁  \n\n⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱  \n\n💨𝕮𝖍𝖆𝖓𝖓𝖊𝖑: @agorihome➻ \n💨𝕮𝖍𝖆𝖓𝖓𝖊𝖑 : @agoriseries➻ \n🚸𝕲𝖗𝖔𝖚𝖕 : @agorimovies ➻ \🚸𝕲𝖗𝖔𝖚𝖕 : @mv_mania”,
+                caption = "❤️Thankyou For Using Our Service Please Support By Sharing Channel/Group Your Friends \n\n❁Join Our Channels❁<b> \n\n⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱⟱  \n\n💨Channel: @agorihome➻ \n💨Channel: @agoriseries➻ \n🚸Group : @agorimovies ➻ \🚸Group : @mv_mania”,
                 parse_mode="html",
                 reply_to_message_id=update.message_id,
                 reply_markup=InlineKeyboardMarkup(
